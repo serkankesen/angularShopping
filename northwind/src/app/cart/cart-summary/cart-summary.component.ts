00f64dfb833339a21a8bfa4a27fb629852abc0f6
serkan kesen <serkankesen@netuce.com>
2017-11-26 19:54:54 +0300

    000 skesen edited cart summary

--- northwind/src/app/cart/cart-summary/cart-summary.component.ts
+++ northwind/src/app/cart/cart-summary/cart-summary.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit, DoCheck } from '@angular/core';
-import { CartService } from '../cart.service';
-import { CartItem } from '../cart-item';
+import { Component, OnInit, DoCheck } from "@angular/core";
+import { CartService } from "../cart.service";
+import { CartItem } from "../cart-item";
+import { Product } from "../../product/product";
 
 @Component({
-  selector: 'app-cart-summary',
-  templateUrl: './cart-summary.component.html',
-  styleUrls: ['./cart-summary.component.css']
+  selector: "app-cart-summary",
+  templateUrl: "./cart-summary.component.html",
+  styleUrls: ["./cart-summary.component.css"]
 })
-export class CartSummaryComponent implements OnInit,DoCheck {
-
-  constructor(private cartService: CartService) { }
-  totalCartItem:number;
-  totalCartItemPrice:number;
-  cartItems:CartItem[];
+export class CartSummaryComponent implements OnInit, DoCheck {
+  constructor(private cartService: CartService) {}
+  totalCartItem: number;
+  totalCartItemPrice: number;
+  cartItems: CartItem[];
 
   ngOnInit() {
-    this.cartItems=this.cartService.list();
+    this.cartItems = this.cartService.list();
   }
 
-  ngDoCheck(){
-    this.totalCartItem = this.cartService.list().reduce((a,b)=>a+b.quantity,0);
-    this.totalCartItemPrice = this.cartService.list().reduce((a,b) => a+b.product.unitPrice*b.quantity,0);
+  ngDoCheck() {
+    this.totalCartItem = this.cartService
+      .list()
+      .reduce((a, b) => a + b.quantity, 0);
+    this.totalCartItemPrice = this.cartService
+      .list()
+      .reduce((a, b) => a + b.product.unitPrice * b.quantity, 0);
   }
-
+  removeFromCart(product: Product) {
+    this.cartService.removeFromCart(product);
+  }
 }
